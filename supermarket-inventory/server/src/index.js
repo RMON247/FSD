f3c8c2178aa72313f3c46f1513315eb2b30c7fc0
RMON247 <2024.rittam.mondal@ves.ac.in>
2026-09-21 18:33:05 +0530

    fixed ratelimiter

--- supermarket-inventory/server/src/index.js
+++ supermarket-inventory/server/src/index.js
@@ -15,6 +15,7 @@ import transactionRoutes from './routes/transactionRoutes.js'
 import { notFoundHandler, errorHandler } from './middleware/errorHandler.js'
 
 const app = express()
+app.set('trust proxy', 1)
 
 // ---- Security & parsing middleware (Experiment 5) ----
 app.use(helmet())
